c0b651d3e21a0b74dca20e41ba3a72bafd3a9179
Nishant5789 <nishantbhandari218@gmail.com>
2022-11-16 22:30:46 +0530

    first commit

--- public/puzzle.js
+++ public/puzzle.js
@@ -1,4 +1,6 @@
-var screen = document.getElementById('img-main');
+// screen.style.display="none";
+
+var screen = document.getElementById('img-main'); // take the reference of html tags
 var puzzle_screen = document.getElementById('puzzle-main');
 var level_screen = document.getElementById('level');
 var result_screen = document.getElementById('result');
@@ -19,10 +21,26 @@ var play_return = document.getElementById('play_return');
 
 var rows = 3;
 var columns = 3;
+puzzle_screen.style.display="none";
+
+level_screen.style.display="none";
+result_screen.style.display="none";
 
 var currTile;
-var otherTile; //blank tile
+var otherTile; 
 var turns = 0;
+
+var audio = new Audio("./level/Game-Menu_Looping.mp3");
+//async is used to deal with promice
+window.onload = async  function() {
+    sound.innerText="Mute sound";
+    try {
+        await audio.play();
+        console.log("Playing audio" + audio);
+    }catch (err) {
+        console.log("Failed to play, error: " + err);
+    }
+  }
 
 var originalorder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
@@ -35,17 +53,12 @@ let items = [ ["1", "2", "3", "4", "5", "9", "7", "8", "6"],
         ["4", "2", "8", "5", "1", "6", "7", "9", "3"],
         ["4", "2", "8", "5", "1", "6", "7", "9", "3"] ];  
 
-// screen.style.display="none";
-puzzle_screen.style.display="none";
-// puzzle_screen.hidden=true;
-level_screen.style.display="none";
-result_screen.style.display="none";
 
 
 
 const board_start = (index) => {
 
-    console.log("board_start");
+    // console.log("board_start");
     level_screen.style.display="none";
     for(let i=0; i<9; i++)
     {
@@ -78,7 +91,6 @@ const board_start = (index) => {
             tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
             tile.addEventListener("dragend", ()=>{dragEnd(index)});      //after drag drop, swap the two tiles
             document.getElementById("puzzle-main").append(tile);
-
         }
     }
 }
@@ -86,19 +98,14 @@ const board_start = (index) => {
 function dragStart() {
     currTile = this; //this refers to the img tile being dragged
 }
-
 function dragOver(e) {
     e.preventDefault();
 }
-
 function dragEnter(e) {
     e.preventDefault();
 }
-
-function dragLeave() {
-    
+function dragLeave() {   
 }
-
 function dragDrop() {
     otherTile = this; //this refers to the img tile being dropped on
 }
@@ -107,6 +114,7 @@ var dragEnd = (index) => {
     if (!otherTile.src.includes(`level/lavel_${index}/9.png`)) {
         return;
     }
+
     let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
     let r = parseInt(currCoords[0]);
     let c = parseInt(currCoords[1]);
@@ -137,47 +145,40 @@ var dragEnd = (index) => {
         // console.log(`${currImg}    ${otherImg}`);
         currTile.src = otherImg;
         otherTile.src = currImg;
-        
+
         turns += 1;
         console.log(turns);
         // document.getElementById("turns").innerText = turns;
         if(JSON.stringify(imgOrder)==JSON.stringify(originalorder))
         {
-            // console.log("yes");
+            console.log("turns: " + turns);
             if(turns<=5)
             {
                 comment_text.innerText="Awesome You Got stars";
                 img_star.src="level/3_star.png";
             }
-            else if(turns<=10)
+            else if(turns>5 && turns<10)
             {
                 comment_text.innerText="Good luck";
                 img_star.src="level/2_star.png";
             }
             else{
-                comment_text.innerText="Good But Next time Improve Your Game";
+                comment_text.innerText="Good Hope next time Improve Your Game";
                 img_star.src="level/1_star.png";
             }
+
             result_screen.style.display="block";
             puzzle_screen.style.display="none";
             
             while (puzzle_screen.firstChild) {
                     puzzle_screen.removeChild(puzzle_screen.firstChild);
             }
-
-
+            turns=0;
             // document.getElementById("comment").innerHTML="Congrats solved";
             // document.getElementById("btn").style.display="block";
         }
     }
 }
-// const board = () => {
-//     console.log("board");
-//     screen.style.display="none";
-//     puzzle_screen.style.display="flex";
-    
-//     board_start(0);
-// }
 
 const board = (index)=> {
     console.log("board");
@@ -199,17 +200,42 @@ const level = (flag) => {
     }
 }
 const start = () =>{
-    // puzzle_screen.style.display="none";
-    // puzzle_screen.hidden=true;
     level_screen.style.display="none";
+}
+const mute_sound = () => {
+    if(sound.innerText=="Mute sound")
+    {
+        console.log("pause");
+        audio.pause();
+        sound.innerText="Unmute sound";
+    }
+    else if(sound.innerText=="Unmute sound")
+    {
+        console.log("play");
+        audio.play();
+        sound.innerText="Mute sound";
+    }
+}
+
+const return_main_menu = ()=>{
+    console.log("return");
+    puzzle_screen.style.display="none";
+    // puzzle_screen.hidden=true;
+    screen.style.display="block";
+    level_screen.style.display="none";
+    result_screen.style.display="none";
 }
 
 play_btn.addEventListener("click", ()=>{board(0)});
+sound.addEventListener("click", ()=>{mute_sound()});
 puzzle_btn.addEventListener("click", ()=>{level(0)} );
 play_level1.addEventListener("click",()=>{board(0)} );
 play_level2.addEventListener("click",()=>{board(1)} );
 play_level3.addEventListener("click",()=>{board(2)} );
 play_level4.addEventListener("click",()=>{board(3)} );
 play_level5.addEventListener("click",()=>{board(4)} );
-return_btn.addEventListener("click",()=>{level(1)} );
+return_btn.addEventListener("click",()=>{return_main_menu()});
+play_return.addEventListener("click",()=>{return_main_menu()});
+
+
 
